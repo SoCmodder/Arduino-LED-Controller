24d7ed9fa20e65bc6ea65ed1bbbb0402448fbc3d
SoCmodder <socmodder@gmail.com>
2022-10-08 19:35:34 -0600

    update colors

--- led-controller/led-controller.ino
+++ led-controller/led-controller.ino
@@ -4,7 +4,9 @@
 #define red 9
 #define green 10
 #define blue 11
-int t = 500;
+int t = 2000;
+int brightness;
+int fadeAmount = 5;
 
 void setup()
 {
@@ -13,45 +15,62 @@ void setup()
 
 void loop()
 {
-  analogWrite(red, 255);
+  showPurple();
   delay(t);
-  analogWrite(green, 255);
+  showDarkOrange();
   delay(t);
-  analogWrite(blue, 255);
-  delay(t);
-  analogWrite(red, 0);
-  delay(t);
-  analogWrite(green, 0);
+  showBlue();
   delay(t);
 }
 
 void showPurple() 
 {
   analogWrite(red, 128);
-  delay(t);
   analogWrite(green, 0);
-  delay(t);
   analogWrite(blue, 128);    
-  delay(t);
+}
+
+void showBlack() {
+  analogWrite(red, 0);
+  delay(100);
+  analogWrite(green, 0);
+  delay(100);
+  analogWrite(blue, 255);
+  delay(100);    
+}
+
+void showBlue() 
+{
+  analogWrite(red, 255);
+  analogWrite(green, 0);
+  analogWrite(blue, 100);    
 }
 
 void showIndigo() 
 {
   analogWrite(red, 75);
-  delay(t);
   analogWrite(green, 0);
-  delay(t);
   analogWrite(blue, 130);    
-  delay(t);
 }
 
 //https://www.rapidtables.com/web/color/orange-color.html
 void showDarkOrange() 
 {
-  analogWrite(red, 255);
-  delay(t);
-  analogWrite(green, 140);
-  delay(t);
-  analogWrite(blue, 0);    
-  delay(t);
+  analogWrite(red, 0);
+  analogWrite(green, 0);
+  analogWrite(blue, 255);    
+}
+
+void fade() {
+  analogWrite(red, brightness); //writing brightness value to red
+  analogWrite(green, brightness); //writing brightness value to green
+  analogWrite(blue, brightness);
+
+  brightness += fadeAmount; //increasing brightness value by 5 in each loop
+
+  if (brightness < 0 || brightness > 255 )
+  {
+    fadeAmount = -fadeAmount; //reversing the direction of fading when brightness reachs 0 or 255
+  }
+  delay(30); //delaying the speed of fading   
 }
